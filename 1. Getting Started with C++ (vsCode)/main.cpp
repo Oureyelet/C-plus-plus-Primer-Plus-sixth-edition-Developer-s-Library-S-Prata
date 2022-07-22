060cac6c6af49c43fbe49e530627e43a60047f3c
Mateusz <oureyelet@gmail.com>
2022-07-22 19:11:03 +0100

    :P

--- 1. Getting Started with C++ (vsCode)/main.cpp
+++ 1. Getting Started with C++ (vsCode)/main.cpp
@@ -2,7 +2,15 @@
 
 int main()
 {
-    std::cout << "Hello World" << '\n';
+    std::cout << "Hello World my name is Sophie\n";
+
+    int a{ 5 };
+
+    for(int count{ 0 }; count < 50; count++)
+    {
+        std::cout << a << ' ';
+        a++;
+    }
 
     return 0;
 }
