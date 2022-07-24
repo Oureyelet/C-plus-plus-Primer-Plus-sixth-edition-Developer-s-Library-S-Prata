d28cf2ec7793632e48a15cf761915dee928aabcb
Mateusz <oureyelet@gmail.com>
2022-07-24 07:55:31 +0100

    Let's be beginning :) !

---
+++ 1. Getting Started with C++ (vsCode)/Pillars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int pillars(int number_of_pillars, int distance_between_pillars, int width_of_the_pillar)
+{
+    return 
+}
+
+int main()
+{
+
+
+    return 0;
+}
--- 1. Getting Started with C++ (vsCode)/main.cpp
+++ 1. Getting Started with C++ (vsCode)/main.cpp
@@ -1,16 +1,59 @@
 #include <iostream>
+#include <string>
+#include <string_view>
+
+class Example
+{
+private:
+
+    std::string m_name{};
+    std::string m_title{};
+    double m_salary{};
+    std::string m_unusual_abilities{};
+
+public:
+    Example(std::string_view name, std::string_view title, double salary, std::string_view un_abi)
+        : m_name{ name }, m_title{ title }, m_salary{ salary }, m_unusual_abilities{ un_abi }
+    {
+        std::cout << name << ' ' << title << " $" << salary << ' ' << un_abi << '\n'; 
+    }
+
+};
+
+template<typename T>
+T add(T a, T b)
+{
+    return a + b;
+}
 
 int main()
 {
     std::cout << "Hello World my name is Sophie\n";
 
-    int a{ 5 };
+    //Learning C++: What lies before you
 
-    for(int count{ 0 }; count < 50; count++)
-    {
-        std::cout << a << ' ';
-        a++;
-    }
+    //The Origins of c++: A little History
+    
+    //The C Language
+
+    //C Programming Philosophy
+
+    //The C++ Shift: Object-Oriented Programing1
+        Example example{ "Guilford Sheepbla", "Vice President", 925.00, "Windows registry" }; 
+
+    //C++ and generic programing
+    std::cout << add<double>(2, 5.2) << '\n';
+
+    //The Genesis of C++
+    int c{ 6 };
+
+    c++;
+
+    std::cout << c << '\n';
+
+    //Portability and Standards
+
+    //Language growth
 
     return 0;
 }
