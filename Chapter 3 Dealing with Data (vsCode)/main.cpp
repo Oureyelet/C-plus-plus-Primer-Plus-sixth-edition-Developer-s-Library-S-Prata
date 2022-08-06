44b0c6a9216e7e2c3921f77f926793942d88abc0
Mateusz <oureyelet@gmail.com>
2022-08-06 11:04:01 +0100

    nice one :)

--- Chapter 3 Dealing with Data (vsCode)/main.cpp
+++ Chapter 3 Dealing with Data (vsCode)/main.cpp
@@ -183,11 +183,94 @@ int main()
     //-----------------------------------
     // 'signed char' and 'unsigned char':
     //-----------------------------------
-    char fodo;
-    unsigned char bar;
-    singed char snark;
+    char fodo;          // may be signed, may be unsigned
+    unsigned char bar;  // definitely unsigned
+    signed char snark;  // definitely signed
+
+
+    //---------------------------------------------------------
+    // For When You Need More - wchar_t (wide character type) :
+    //---------------------------------------------------------
+    wchar_t bob = L'P';                 // a wide-character constant
+    std::wcout << L"tall" << std::endl; // outputing a wide-character string
+
+
+    //-----------------------------------------
+    // New C++11 Types - char16_t and char32_t:
+    //-----------------------------------------
+    char16_t ch1{ u'g' };           // basic character in 16-bit form
+    // char32_t ch2 = U'/U0000222B';  // universal character name in 32-bit form
+    
+
+    //-----------------
+    // The 'bool' Type:
+    //-----------------
+    bool is_ready{ true }; 
+
+    int ans{ true };        // ans assigned 1
+    int promise{ false };   // promise assigned 0
+
+    bool start = -100;     // any nonzero value converts to true
+    bool stop{ 0 };         // zero value converts to false
+
+
+    //-----------------------
+    // The 'const' Qualifier:
+    //-----------------------
+    const int months{ 12 }; // Months is symbolic constant fro 12
+
+
+    //------------------------
+    // Floating-Point Numbers:
+    //------------------------
 
     
+    //--------------------------------
+    // Writing Floating-Point Numbers:
+    //--------------------------------
+    float a{ 12.34 };       // floating-point
+    float b{ 939001.32 };   // floating-point
+    float c{ 0.00023 };     // floating-point
+    float d{ 8.0 };         // still floating-point
+
+    std::cout << a << ' ' << b << ' ' << c << ' ' << d << '\n'; // print 12.34 939001 0.00023 8
+
+    /*
+    Note only:
+    E Notation is looks like this: 3.45E6 (3.45 is multiplied by 1,000,000)
+    The E6 means 10 to the 6th power (which is 1 followed by 6 zeros)
+    Thus 3.45E6 means: 3,450,000
+    Here is more examples:
+
+    2.52e+8             // can use E or e, + is optional
+    8.33E-4             // exponent can be negative
+    7E5                 // same as 7.0E+05
+    -18.32e13           // can have + or - sign in front
+    1.69e12             // 2010 Brazilian public debt in reais
+    5.98E24             // mass of earth in kilograms
+    9.11e-31            // mass of an electron in kilograms
+    
+    8.33E-4 means 8.33 / 10 to the 4th power or 0.000833
+    */
+
+
+    //-------------------------------------------------------------------------------------------
+    // Floating-Point Type (C++ has three floating-point types: float, double, and long double) :
+    //-------------------------------------------------------------------------------------------
+    //float.h header file...
+
+
+
+
+
+    //-----------------------------------------------------------
+    //check C++ version:
+    if (__cplusplus == 201703L) std::cout << "C++17\n";
+    else if (__cplusplus == 201402L) std::cout << "C++14\n";
+    else if (__cplusplus == 201103L) std::cout << "C++11\n";
+    else if (__cplusplus == 199711L) std::cout << "C++98\n";
+    else std::cout << "pre-standard C++\n";
+    //-----------------------------------------------------------
 
     std::cin.get();
     std::cin.get();
