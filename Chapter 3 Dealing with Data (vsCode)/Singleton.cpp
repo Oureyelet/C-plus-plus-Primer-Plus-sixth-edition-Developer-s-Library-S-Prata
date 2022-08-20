3c16aef5e5d49a222b1f54d9e4931fa51d0c5e52
Mateusz <oureyelet@gmail.com>
2022-08-20 12:32:06 +0100

    task for job

---
+++ Chapter 3 Dealing with Data (vsCode)/Singleton.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+
+void doWork()
+{
+
+}
+
+int main()
+{
+    std::thread worker(doWork);
+
+    workjghjghjg//mb  1225
+
+
+    \\\\\
+    .j
+    
+    return 0;
+}
