dd95eb5dbe30da335d502fad5b12bd678045f8a3
Mateusz <oureyelet@gmail.com>
2022-08-25 20:31:10 +0100

    restart vs Code...

--- Chapter 3 Dealing with Data (vsCode)/Programming Exercises/1.cpp
+++ Chapter 3 Dealing with Data (vsCode)/Programming Exercises/1.cpp
@@ -10,7 +10,20 @@
 
 int main()
 {
+    // 1 ft = 12 inches
 
+    const int one_ft_is_inches{ 12 };
+    int inches{};
 
+    int feet{ inches / one_ft_is_inches };
+    int inches_remainder{ inches % one_ft_is_inches }; // remainder in inches
+    
+    std::cout << "Enter you height in inches please_: ";
+    std::cin >> inches; // 5 
+
+    std::cout << inches << " inches is " << feet << " feet, and " << inches_remainder << " in.\n";
+
+    std::cin.get();
+    std::cin.get();
     return 0;
 }
