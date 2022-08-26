9f862f72b23ce5e82ba64c8d93cd89c209832ea2
Mateusz <oureyelet@gmail.com>
2022-08-26 21:10:39 +0100

    All tasks are ready to do.

---
+++ Chapter 3 Dealing with Data (vsCode)/Programming Exercises/6.cpp
@@ -0,0 +1,18 @@
+/**
+    Chapter 3 -- Programming Exercises:
+
+    6)  Write a program that asks how many miles you have driven and how many gallons
+        of gasoline you have used and then reports the miles per gallon your car has gotten.
+        Or, if you prefer, the program can request distance in kilometers and petrol in liters
+        and then report the result European style, in liters per 100 kilometers.
+*/
+#include <iostream>
+
+int main()
+{
+    
+
+    std::cin.get();
+    std::cin.get();
+    return 0;
+}
