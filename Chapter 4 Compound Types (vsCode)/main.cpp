b144675c60b0c0982a3ebdb919fffc707eca357c
Mateusz <oureyelet@gmail.com>
2022-08-31 20:28:54 +0100

    just for activity :)

--- Chapter 4 Compound Types (vsCode)/main.cpp
+++ Chapter 4 Compound Types (vsCode)/main.cpp
@@ -17,7 +17,13 @@ int main()
     // Program Notes:
 
     // Initialization Rules for Arrays:
-        
+    int card[4] = { 3, 6, 8, 10 };      // okay
+    int hands[4];                       // okay
+    float hotelTips[5] = { 5.0, 2.5 };  // initializes only the first two elements of hotelTips.
+    long totals[500] = { 0 };           // initialize the first element to zero and then let the compiler initialize the remaining elements to zero
+    short things[] = { 1, 4, 6, 8 };    // The compiler makes things an array of four elements.
+
+        // Letting the Compiler Do It:
 
     return 0;
 }
